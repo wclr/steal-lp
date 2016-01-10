5580f51d44a465e201c2a6f84a14ab121ea2fe2f
Alex <alex.oschepkov@gmail.com>
2016-01-10 19:45:31 +0500

    turn to L-packs

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-##Language packs for StealJS apps.
+##L-packs for StealJS apps.
 
-Loads language (locales) dictionary as function that takes language and returns promise to fulfil with dictionary data.
+L is for Language, Locale, Label, Lemming, Lenin, etc... 
+
+Loads L-dictionary as function that takes L-name and returns promise to fulfil with dictionary data.
 
 ##How to use
 
 Dictionaries are stored as separate CJS modules for every language usually per component:
 
-`my-component/dict/en.js:`
+my-component/dict/en.js:
 ```javascript
 module.exports = {
   name: 'name'
 }
 ```
 
-`my-component/dict/es.js:`
+my-component/dict/es.js:
 ```javascript
 module.exports = {
   name: 'nombre'
@@ -28,15 +30,20 @@ dict('es').then(esDictData => {
   console.log(esDictData.name) // -> nombre
 })
 ```
-Just install it from npm `npm install steal-lp` (when published) and for simple use map in system config:
+Just install it from npm `npm install steal-lp` (if available) or install from this repo for now and for simple use map in system config:
 ```json
 "map": {
-  `"./lp": "steal-lp"`
+  "./lp": "steal-lp"
 }
 ```
-If install from repo you may need to map `"./lp": "./node_modules/steal-lp/lp"` (StealJS bug) 
-
-While build `steal-tools` will create language-packs with `*.en.lp` extension (for every language) per bundle. 
+ 
+If you want to use another type just map it (and use for import):
+```json
+"map": {
+  "./language.pack": "steal-lp"
+}
+```
+While build `steal-tools` will create language-packs with (for example) `app-bundle.en-us.language.pack` extension (for every L-name) per bundle. 
 So particular language bundle loads in production when it is only needed.
 
 ##Example app
@@ -48,5 +55,3 @@ Can be found in `test-app` folder
 - check test-app/index.html - dev version
 - node build.js - built (dist) version
 - check test-app/index.dist.html
-
-
--- lp.js
+++ lp.js
@@ -1,5 +1,5 @@
 // Browser loader that will load !lp module as function that returns Promise
-var getWindowLoader = function(name, address, load){
+var getWindowLoader = function(name, address, load, lpType){
   return `
     /** @hot-deps **/
     module.exports = function(lang){
@@ -7,7 +7,7 @@ var getWindowLoader = function(name, address, load){
         console.warn('[steal-lp] illegal lang value for module ${name}:', lang)
         return new Promise.resolve({})
       }
-      var moduleName = "${name}" + "/" + lang + ".js!" + lang + ".lp"
+      var moduleName = "${name}" + "/" + lang + ".js!" + lang + ".${lpType}"
       var loaded = System.import(moduleName)
       // modify System._traceData for System.getDependants to work
       System._traceData && loaded.then(function(){
@@ -24,17 +24,17 @@ var getWindowLoader = function(name, address, load){
 }
 
 // Node loader that will load !lp module as function that returns Promise
-// it uses defaultLang and fallbackLang options from System.lp config
-var getNodeLoader = function(name, address, load){
+// it uses `default` and `fallback` options from System.lp config
+var getNodeLoader = function(name, address, load, lpType){
   var fs = require('fs')
   var langs = fs.readdirSync(address).map((f) => f.split('.')[0])
 
   load.metadata.deps = langs.map((lang) =>
-    name + '/' + lang + '.js!' + lang + '.lp'
+    name + '/' + lang + '.js!' + lang + '.' + lpType
   )
-  var lpConfig = System.lp || {},
-    defaultLang = lpConfig.defaultLang || '',
-    fallback = !!lpConfig.fallbackLang
+  var lpConfig = (System.lp && System.lp[lpType]) || System.lp || {},
+    defaultLang = lpConfig.default || '',
+    fallback = !!lpConfig.fallback
 
   langs = langs.map((lang) => '"' + lang + '"').join()
 
@@ -51,24 +51,36 @@ var getNodeLoader = function(name, address, load){
           }
         }
       }
-      return System.import("${name}" + "/" + lang + ".js!" + lang + ".lp")
+      return System.import("${name}" + "/" + lang + ".js!" + lang + ".${lpType}")
     }
     `
 }
 
+var lpTypes = {}
+var lpMatchRegExp
+
+var addLpType = function(lpType){
+  if (lpTypes[lpType]) return
+  lpTypes[lpType] = true
+  var types = Object.keys(lpTypes).join('|')
+  lpMatchRegExp = new RegExp(`([\\w\\d-]+)\\.(${types})\\.js$`, 'i')
+}
+
 var matchLangPlugin = function(address){
-  var match = address.match(/([\w\d-]+)\.lp\.js$/i)
-  return match && match[1]
+
+  var match = lpMatchRegExp && address.match(lpMatchRegExp)
+  match && match.shift()
+  return match
 }
 
 // Should use @loader in steal build process, not System
 import loader from '@loader'
 
-var oldFetch = loader.fetch;
+var _fetch = loader.fetch;
 
 loader.fetch = function(load) {
-  var lang = matchLangPlugin(load.address)
-  if (lang){
+  var match = matchLangPlugin(load.address)
+  if (match){
     // for steal build return plugin code for loading lang modules
     if (typeof window === 'undefined'){
       return `
@@ -76,14 +88,14 @@ loader.fetch = function(load) {
           return 'define("' + load.name + '", function(require, exports, module){' +
           '' + load.source + '});'
         }
-        export let buildType = '${lang}.lp'
+        export let buildType = '${match[0]}.${match[1]}'
         `
     } else {
       // for browser return empty plugin, with hot-reload support
       return '/** @hot-deps **/'
     }
   } else {
-    return oldFetch.call(this, load)
+    return _fetch.apply(this, arguments)
   }
 }
 
@@ -92,9 +104,20 @@ export function fetch(load) {
   let name = load.name.replace(/!.*$/, '').replace(/.*#/, '')
   let address = load.address.replace(/^file:/, '').replace(/\\/g, '/')
 
+  let lpType = 'lp'
+  // In node loader.getDependants returns empty result, while System works
+  if (System.getDependants){
+    var loads = System.loads || (System._traceData && System._traceData.loads)
+    var parent = System.getDependants(load.name)[0]
+    var meta = loads && parent && loads[parent] && loads[parent].metadata
+    var dep = meta && meta.deps[meta.dependencies.indexOf(load.name)]
+    dep && (lpType =  dep.match(/!(.*)$/)[1])
+  }
+  addLpType(lpType)
+
   if (typeof window === 'undefined'){
-    return getNodeLoader(name, address, load)
+    return getNodeLoader(name, address, load, lpType)
   } else {
-    return getWindowLoader(name, address, load)
+    return getWindowLoader(name, address, load, lpType)
   }
 }
